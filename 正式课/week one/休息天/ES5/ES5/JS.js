d7bca063456c11dc5efad6d2b7353d3325d8b2e0
jumpingFinger <724015930@qq.com>
2018-04-20 15:04:22 +0800

    2018-04-19

---
+++ 正式课/week one/休息天/ES5/ES5/JS.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// let {name , ...arg}={name : '珠峰培训',age :9};
+// console.log(name,arg); //'珠峰培训' Object{age :9,};
+var a = 11;
---
+++ 正式课/week one/休息天/ES6/JS.js
@@ -0,0 +1,3 @@
+// let {name , ...arg}={name : '珠峰培训',age :9};
+// console.log(name,arg); //'珠峰培训' Object{age :9,};
+let a=11;
